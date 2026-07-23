c560ad59a92270260caab4315a4149e1b41ba735
Eric Tao <shuaibtcm@gmail.com>
2026-07-23 19:35:23 +0100

    Add: implement-queue-using-stacks - submission-0

---
+++ Data Structures & Algorithms/implement-queue-using-stacks/submission-0.java
@@ -0,0 +1,31 @@
+class MyQueue {
+    private ArrayDeque<Integer> q = new ArrayDeque();
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        q.addLast(x);
+    }
+    
+    public int pop() {
+        return q.pop();
+    }
+    
+    public int peek() {
+        return q.peek();
+    }
+    
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
