c134411c484538e50bd9d8264fbdd69b3032776a
Eric Tao <shuaibtcm@gmail.com>
2026-07-24 03:11:19 +0100

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k %= n;
+        reverse(nums, 0, n - 1);        
+        reverse(nums, 0, k - 1);        
+        reverse(nums, k, n - 1);        
+    }
+
+    public void reverse(int[] nums, int l, int r) {
+        while (l < r) {
+            int tmp = nums[l];
+            nums[l] = nums[r];
+            nums[r] = tmp;
+            l++;
+            r--;
+        }
+    } 
+}
